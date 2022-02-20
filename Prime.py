9ccb5e767008f1dbb1c0e079178446f332aa4647
TEEPARTHILSSNAGAVALLI <99274565+TEEPARTHILSSNAGAVALLI@users.noreply.github.com>
2022-02-20 09:14:20 +0530

    Create Prime.py

---
+++ Prime.py
@@ -0,0 +1,14 @@
+t = int(input())
+c=0
+s=0
+for i in range(n):
+    n = input()
+    n = int(n)
+    for j in range(2,n+1):
+        for k in range(1,j+1):
+             if i%j==0:
+                 c=c+1
+        if c==2:
+             s=s+1
+        c=0
+    print(s)
