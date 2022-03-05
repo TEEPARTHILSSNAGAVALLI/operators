f28b200b29b39a20441fabe7e81c1c2a44b2bd41
TEEPARTHILSSNAGAVALLI <99274565+TEEPARTHILSSNAGAVALLI@users.noreply.github.com>
2022-03-05 07:16:52 +0530

    Update runnerup.py

--- runnerup.py
+++ runnerup.py
@@ -1,1 +1,9 @@
-
+l = []
+n = int(input("Enter the size of list:"))
+print("Enter the data into the list:")
+for i in range(n):
+    x = int(input())
+    l.append(x)
+l.sort()
+l.reverse()
+print(l[1])
